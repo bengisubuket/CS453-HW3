8e27da101908a5449ce2d83ba0cf1fbc40140a86
bengisubuket <bengisubuket@icloud.com>
2024-04-18 16:06:55 +0300

    a Syntax Error introduced in setup.py

--- setup.py
+++ setup.py
@@ -3,14 +3,12 @@ from setuptools import setup
 setup(
     # TODO: Write a globally unique name which will be listed on PyPI
     name="very-simple-dictionary",
-    author="bengisubuket",  # TODO: Write your name
+    author="bengisubuket"
     version="2.0.0",
     packages=["dictionary"],
     install_requires=[
         "requests>=2.23.0",
     ],
-    python_requires=">=3.8", 
-    # Introduce an invalid setup argument
-    non_existent_argument="This will cause an error",
+    python_requires=">=3.8",
 
 )
